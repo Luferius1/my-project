760f978e86ebec315c7f0f3daf024d33afed25fe
Luferius1 <110639483+Luferius1@users.noreply.github.com>
2024-05-27 22:43:06 +0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-
-    def __repr__(self):
-        return f'<Note {self.title}>'
